ab0c023b371facf06bdd09d462c0bdbb03b0acf9
NelsonQ <nelson_eq@yahoo.com>
2021-02-04 18:53:53 -0800

    Favorite Food

---
+++ GuessingGame.py
@@ -0,0 +1,13 @@
+#ask what is your favorite food
+guess = input ("What's your favorite food? ")
+
+def new_func(guess):
+    if guess == "Noodles":
+        print ("Yep! That's so amazing!")
+    else:
+        print ("Yuck! That's not it!")
+
+new_func(guess)
+
+print ("Thanks for playing!")
+
